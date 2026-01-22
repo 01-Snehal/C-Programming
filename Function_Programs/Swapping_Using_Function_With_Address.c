473d1acd07176dc3bd64e427de6e1c026c7c409b
Snehal Shahaji Bhandare <snehalbhandare0117@gmail.com>
2026-01-22 16:08:24 +0530

    Function Programs

---
+++ Function_Programs/Swapping_Using_Function_With_Address.c
@@ -0,0 +1,37 @@
+/// Write a program to print swapping of two numbers by using Function with Address.
+
+#include<stdio.h>
+#include<conio.h>
+
+void Swap(int* , int*);
+
+int main()
+{
+    int N1 = 0;
+    int N2 = 0;
+
+    printf("\n Enter First Number: ");
+    scanf("%d",&N1);
+
+    printf("\n Enter Second Number: ");
+    scanf("%d",&N2);
+
+    printf("\n\n Values Before Swapping : %d ,%d ",N1,N2);
+
+    Swap(&N1,&N2);
+
+    printf("\n\n Values After Swapping : %d ,%d ",N1,N2);
+
+    getch();
+    return 0;
+
+}
+
+void Swap(int* No1, int* No2)
+{
+    int Temp = 0;
+
+    Temp = *No1;
+    *No1 = *No2;
+    *No2 = Temp;
+}
